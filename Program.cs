7a8dcc66870c36cfb8b2a6ed02f8450658462774
Aleksey <kukoba.alexey@gmail.com>
2023-07-18 15:55:33 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -4,3 +4,28 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a1 = num / 10000;
+int a2 = num / 1000 % 10;
+int a4 = num / 10 % 10;
+int a5 = num % 10;
+
+// Console.WriteLine($"{a1} {a2} {a4} {a5}");
+if(num >= 10000 && num <=99999)
+{
+    if(a1 == a5 && a2 == a4)
+    {
+        Console.Write("Да");
+    }
+    else
+    {
+        Console.Write("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
